bc247cdd090cbf249ed848aa1a5bb48bb90ef7df
NamanSondhiya <namansondhiya17@gmail.com>
2025-09-29 16:01:36 +0530

    gitleaks updted

--- vars/gitleaks_scan.groovy
+++ vars/gitleaks_scan.groovy
@@ -1,3 +1,3 @@
-def call() {
-	sh 'gitleaks detect --source . -r gitleaks-report.json -f json'
+def call(boolean "break") {
+	sh 'gitleaks detect --source . -r gitleaks-report.json -f json || "${break}"'
 }
