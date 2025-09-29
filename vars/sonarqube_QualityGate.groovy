c86a032ce51a1807c34c97f5bf9edda3ec0df116
NamanSondhiya <namansondhiya17@gmail.com>
2025-09-29 15:36:52 +0530

    versatality added:v1

---
+++ vars/sonarqube_QualityGate.groovy
@@ -0,0 +1,5 @@
+def call(int time, boolean abortPipeline) {
+  timeout(time: time, unit: "MINUTES"){
+      waitForQualityGate abortPipeline: "$(abortPipeline)"
+  }
+}
--- vars/sonarqube_qualityGates.groovy
+++
@@ -1,5 +0,0 @@
-def call(String abortPipeline){
-  timeout(time: 1, unit: "MINUTES"){
-      waitForQualityGate abortPipeline: "$(abortPipeline)"
-  }
-}
