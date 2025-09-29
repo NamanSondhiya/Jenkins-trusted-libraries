79a655f91fae7df0395b0882583690d1bd774c75
NamanSondhiya <namansondhiya17@gmail.com>
2025-09-29 21:29:34 +0530

    venv exlusions added

--- vars/sonarqube_analysis.groovy
+++ vars/sonarqube_analysis.groovy
@@ -6,7 +6,7 @@ def call(String Envir, String projectName, String projectKey) {
                 -Dsonar.projectName=${projectName} \
                 -Dsonar.projectKey=${projectKey} \
                 -Dsonar.sources=. \
-                -DSonar.exclusions=venv/**, node_modules/**
+                -DSonar.exclusions=venv/**, node_modules/** \
                 -Dsonar.host.url=\${SONAR_HOST_URL:-http://localhost:9000} \
                 -Dsonar.login=\${SONAR_TOKEN}
                 """
