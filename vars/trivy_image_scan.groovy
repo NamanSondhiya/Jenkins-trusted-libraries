bc3c17943ecaccd62484d49d962fee85d407ec8c
NamanSondhiya <namansondhiya17@gmail.com>
2025-10-01 16:28:56 +0530

    added json string to the trivy image -o

--- vars/trivy_image_scan.groovy
+++ vars/trivy_image_scan.groovy
@@ -1,6 +1,7 @@
 def call(String Project, String Imagetag, String DOCKERHUB_USER) {
     sh  """
         trivy image --skip-version-check -f table -o trivy-image.txt ${DOCKERHUB_USER}/${Project}:${Imagetag}
+        trivy image --skip-version-check -f json -o tirvy-image.json ${DOCKERHUB_USER}/${Project}:${Imagetag}
         trivy image --skip-version-check --exit-code 1 --severity CRITICAL,HIGH ${DOCKERHUB_USER}/${Project}:${Imagetag}
         """
 }
