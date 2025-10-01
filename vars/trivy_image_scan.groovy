e9b755bc9f72a5000738e236e2d0178af127243f
NamanSondhiya <namansondhiya17@gmail.com>
2025-10-01 15:26:33 +0530

    Added Version skip into image scan

--- vars/trivy_image_scan.groovy
+++ vars/trivy_image_scan.groovy
@@ -1,6 +1,6 @@
 def call(String Project, String Imagetag) {
     sh  """
-        trivy image -f table -o trivy-image.txt ${Project}:${Imagetag}
-        trivy image --exit-code 1 --severity CRITICAL,HIGH ${Project}:${Imagetag}
+        trivy image -f --skip-check-update table -o trivy-image.txt ${Project}:${Imagetag}
+        trivy image --skip-check-update --exit-code 1 --severity CRITICAL,HIGH ${Project}:${Imagetag}
         """
 }
