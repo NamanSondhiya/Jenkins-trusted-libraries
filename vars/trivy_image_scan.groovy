b56da5ca870e6b7e834d61e2943d388dd43c52d0
NamanSondhiya <namansondhiya17@gmail.com>
2025-10-01 16:18:16 +0530

    Added Dockerhub String to trivy image scan

--- vars/trivy_image_scan.groovy
+++ vars/trivy_image_scan.groovy
@@ -1,6 +1,6 @@
-def call(String Project, String Imagetag) {
+def call(String Project, String Imagetag, String DOCKERHUB_USER) {
     sh  """
-        trivy image --skip-version-check -f table -o trivy-image.txt ${Project}:${Imagetag}
-        trivy image --skip-version-check --exit-code 1 --severity CRITICAL,HIGH ${Project}:${Imagetag}
+        trivy image --skip-version-check -f table -o trivy-image.txt ${DOCKERHUB_USER}/${Project}:${Imagetag}
+        trivy image --skip-version-check --exit-code 1 --severity CRITICAL,HIGH ${DOCKERHUB_USER}/${Project}:${Imagetag}
         """
 }
