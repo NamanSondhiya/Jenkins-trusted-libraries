25b0e84488de9e1642c652f632268382cd278203
NamanSondhiya <namansondhiya17@gmail.com>
2025-10-01 15:42:17 +0530

    trivy image scan typo

--- vars/trivy_image_scan.groovy
+++ vars/trivy_image_scan.groovy
@@ -1,6 +1,6 @@
 def call(String Project, String Imagetag) {
     sh  """
-        trivy image -f --skip-check-update table -o trivy-image.txt ${Project}:${Imagetag}
+        trivy image --skip-check-update -f table -o trivy-image.txt ${Project}:${Imagetag}
         trivy image --skip-check-update --exit-code 1 --severity CRITICAL,HIGH ${Project}:${Imagetag}
         """
 }
