e8f4931906fdc5c0ac9a14f69d2d9a7d411e2614
NamanSondhiya <namansondhiya17@gmail.com>
2025-10-01 15:46:00 +0530

    skip version update to skip version check update

--- vars/trivy_image_scan.groovy
+++ vars/trivy_image_scan.groovy
@@ -1,6 +1,6 @@
 def call(String Project, String Imagetag) {
     sh  """
-        trivy image --skip-check-update -f table -o trivy-image.txt ${Project}:${Imagetag}
-        trivy image --skip-check-update --exit-code 1 --severity CRITICAL,HIGH ${Project}:${Imagetag}
+        trivy image --skip-version-check -f table -o trivy-image.txt ${Project}:${Imagetag}
+        trivy image --skip-version-check --exit-code 1 --severity CRITICAL,HIGH ${Project}:${Imagetag}
         """
 }
