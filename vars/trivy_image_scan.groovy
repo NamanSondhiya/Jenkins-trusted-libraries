4c9c35ca023f71f154f1f99af455dae533612f10
NamanSondhiya <namansondhiya17@gmail.com>
2025-10-09 16:52:23 +0530

    trivy image report update

--- vars/trivy_image_scan.groovy
+++ vars/trivy_image_scan.groovy
@@ -1,7 +1,7 @@
 def call(String Project, String Imagetag, String DOCKERHUB_USER) {
     sh  """
-        trivy image --skip-version-check -f table -o trivy-image.txt ${DOCKERHUB_USER}/${Project}:${Imagetag}
-        trivy image --skip-version-check -f json -o tirvy-image.json ${DOCKERHUB_USER}/${Project}:${Imagetag}
+        trivy image --skip-version-check -f table -o ${Project}-trivy-image-report.txt ${DOCKERHUB_USER}/${Project}:${Imagetag}
+        trivy image --skip-version-check -f json -o ${Project}-tirvy-image-report.json ${DOCKERHUB_USER}/${Project}:${Imagetag}
         trivy image --skip-version-check --exit-code 0 --severity CRITICAL,HIGH ${DOCKERHUB_USER}/${Project}:${Imagetag}
         """
 }
