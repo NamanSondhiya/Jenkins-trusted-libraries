ddb1eb1e840ba4c9b2652848f2f5a32df47668e6
NamanSondhiya <namansondhiya17@gmail.com>
2025-10-10 13:56:38 +0530

    from single to double commas

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,8 +1,8 @@
-def call(String project, String imageTag, String dockerUserParam) {
+def call(String Project, String ImageTag, String DockerUserParam) {
     withCredentials([usernamePassword(credentialsId: 'dockerhub-creds', usernameVariable: 'DOCKERHUB_USER', passwordVariable: 'DOCKERHUB_PASS')]) {
-        sh '''
-        echo "$DOCKERHUB_PASS" | docker login -u "$DOCKERHUB_USER" --password-stdin
-        docker push "$DOCKERHUB_USER/${project}:${imageTag}"
-        '''
+        sh """
+        echo "\$DOCKERHUB_PASS" | docker login -u "\$DOCKERHUB_USER" --password-stdin
+        docker push "\$DOCKERHUB_USER/${Project}:${ImageTag}"
+        """
     }
 }
