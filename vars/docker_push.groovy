fbb2c1b8d3e69a5c88b4fe1ee94f8d2eb031c1bc
NamanSondhiya <namansondhiya17@gmail.com>
2025-10-10 13:33:01 +0530

    redid docker push + passstdin

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,6 +1,8 @@
-def call(String Project, String ImageTag, String DOCKERHUB_USER){
-  withCredentials([usernamePassword(credentialsId: 'dockerhub-creds', passwordVariable: 'DOCKERHUB_PASS', usernameVariable: 'DOCKERHUB_USER')]) {
-      sh "docker login -u ${DOCKERHUB_USER} -p ${DOCKERHUB_PASS}  --password-stdin"
-  }
-  sh "docker push ${DOCKERHUB_USER}/${Project}:${ImageTag}"
+def call(String project, String imageTag, String dockerUserParam) {
+    withCredentials([usernamePassword(credentialsId: 'dockerhub-creds', usernameVariable: 'DOCKERHUB_USER', passwordVariable: 'DOCKERHUB_PASS')]) {
+        sh '''
+        echo "$DOCKERHUB_PASS" | docker login -u "$DOCKERHUB_USER" --password-stdin
+        docker push "$DOCKERHUB_USER/${project}:${imageTag}"
+        '''
+    }
 }
