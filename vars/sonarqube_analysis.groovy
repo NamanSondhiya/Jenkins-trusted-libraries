54a3fea3da041456e9a5f2a4d8a48def8a8e40e2
NamanSondhiya <namansondhiya17@gmail.com>
2025-10-14 13:30:24 +0530

    error updates bash and sh

--- vars/sonarqube_analysis.groovy
+++ vars/sonarqube_analysis.groovy
@@ -1,15 +1,17 @@
 def call(String enVir, String projectName, String projectKey, String lang = null) {
     withSonarQubeEnv('Sonar') {
         withCredentials([string(credentialsId: "SonarToken", variable: 'SONAR_TOKEN')]) {
-            sh '''
-                ${enVir}/bin/sonar-scanner \
-                -Dsonar.projectName=${projectName} \
-                -Dsonar.projectKey=${projectKey} \
-                -Dsonar.sources=. \
-                ${lang ? "-Dsonar.language=${lang}" : ""} \
-                -Dsonar.host.url=${SONAR_HOST_URL} \
-                -Dsonar.login=${SONAR_TOKEN}
-            '''
+            def sonarHost = SONAR_HOST_URL?.trim()
+            sh(script: """
+                #!/bin/bash
+                "${enVir}/bin/sonar-scanner" \
+                  -Dsonar.projectName="${projectName}" \
+                  -Dsonar.projectKey="${projectKey}" \
+                  -Dsonar.sources=. \
+                  ${lang ? "-Dsonar.language=${lang}" : ""} \
+                  -Dsonar.host.url="${sonarHost}" \
+                  -Dsonar.login="$SONAR_TOKEN"
+            """, shell: 'bash')
         }
     }
 }
