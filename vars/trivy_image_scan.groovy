2178df0ccd96d0e74f080ed938cade5cfd7c188b
NamanSondhiya <namansondhiya17@gmail.com>
2025-10-16 16:41:28 +0530

    increase time & cache type scans introduced

--- vars/trivy_image_scan.groovy
+++ vars/trivy_image_scan.groovy
@@ -1,7 +1,9 @@
 def call(String Project, String Imagetag, String DOCKERHUB_USER) {
     sh  """
-        trivy image --skip-version-check -f table -o ${Project}-trivy-image-report.txt ${DOCKERHUB_USER}/${Project}:${Imagetag}
-        trivy image --skip-version-check -f json -o ${Project}-tirvy-image-report.json ${DOCKERHUB_USER}/${Project}:${Imagetag}
-        trivy image --skip-version-check --exit-code 0 --severity CRITICAL,HIGH ${DOCKERHUB_USER}/${Project}:${Imagetag}
-        """
+        TRIVY_CACHE_DIR=/tmp/trivy-cache
+
+        trivy image --skip-version-check --cache-dir \$TRIVY_CACHE_DIR --timeout 10m -f table -o ${Project}-trivy-image-report.txt ${DOCKERHUB_USER}/${Project}:${Imagetag}
+        trivy image --skip-version-check --cache-dir \$TRIVY_CACHE_DIR --timeout 10m -f json -o ${Project}-trivy-image-report.json ${DOCKERHUB_USER}/${Project}:${Imagetag}
+        trivy image --skip-version-check --cache-dir \$TRIVY_CACHE_DIR --timeout 10m --exit-code 0 --severity CRITICAL,HIGH ${DOCKERHUB_USER}/${Project}:${Imagetag}
+    """
 }
