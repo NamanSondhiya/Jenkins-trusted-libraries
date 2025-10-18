120e87eca486c633bf4158c2ac7b694a5058bd15
NamanSondhiya <namansondhiya17@gmail.com>
2025-10-18 18:10:21 +0530

    added git push for CD builds GITOPS

---
+++ vars/git_push.groovy
@@ -0,0 +1,12 @@
+def call(String GitUrl, String GitBranch) {
+    withCredentials([usernamePassword(credentialsId: 'github-creds', usernameVariable: 'GIT_USER', passwordVariable: 'GIT_TOKEN')]) {
+        sh """
+        git config --global user.email "ssnaman4@gmail.com"
+        git config --global user.name "jenkins"
+        git remote set-url origin https://${GIT_USER}:${GIT_TOKEN}@${GitUrl}
+        git add .
+        git commit -m "Updated image tags via Jenkins-CD build ${BUILD_NUMBER}" || echo "No changes to commit"
+        git push origin ${GitBranch}
+        """
+    }
+}
