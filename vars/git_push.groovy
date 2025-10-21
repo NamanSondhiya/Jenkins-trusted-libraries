77e1d4364ae99d47b2f9f81de6e6e7ef3bf8cc65
NamanSondhiya <namansondhiya17@gmail.com>
2025-10-21 15:36:14 +0530

    double https// error fix 1

--- vars/git_push.groovy
+++ vars/git_push.groovy
@@ -3,10 +3,10 @@ def call(String GitUrl, String GitBranch) {
         sh """
         git config --global user.email "ssnaman4@gmail.com"
         git config --global user.name "jenkins"
-        git remote set-url origin https://${GIT_USER}:${GIT_TOKEN}@${GitUrl}
+        git remote set-url origin https://${GIT_USER}:${GIT_TOKEN}@${GitUrl.replaceFirst(/^https?:\\/\\//, '')}
         git add .
         git commit -m "Updated image tags via Jenkins-CD build ${BUILD_NUMBER}" || echo "No changes to commit"
-        git push https://${GIT_USER}:${GIT_TOKEN}@${GitUrl} ${GitBranch}
+        git push https://${GIT_USER}:${GIT_TOKEN}@${GitUrl.replaceFirst(/^https?:\\/\\//, '')} ${GitBranch}
         """
     }
 }
